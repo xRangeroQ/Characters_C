8ed772cfec5c1b63707e329c9ae993ebd0207b01
xRangeroQ <82023366+xRangeroQ@users.noreply.github.com>
2026-02-16 20:20:58 +0300

    Add main function to print ASCII characters

---
+++ Source/main.c
@@ -0,0 +1,15 @@
+// Libraries
+#include <stdio.h>
+
+// Main Function
+int main()
+{
+  // For Loop
+  for (int ch = 0; ch < 256; ch++)
+  {
+    printf("Number: %d | Character: %c\n", ch, (char)ch);
+  }
+
+  // Return Function
+  return 0;
+}
